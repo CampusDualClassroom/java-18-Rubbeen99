22fdb3eeeeec20ed9fe6e9a88a1a225f5c694701
rubbeen99 <ruben20042000@gmail.com>
2025-01-17 14:12:20 +0100

    ejercicio 18 resuelto(correcion).

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,40 +1,34 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
 public class Exercise18 {
 
 	public static void main(String[] args) {
-		System.out.println("Introduce el tamaño del array:");
-		java.util.Scanner scanner = new java.util.Scanner(System.in);
-		int size = scanner.nextInt();
-		scanner.close();
-
-		int[] array = createAndInitializeArray(size);
-
-		showInlineArray(array);
+		int length;
+		int[] array;
+		showInlineArray(createAndInitializeArray(Utils.integer("Escriba el tamaño del array: ")));
 	}
 
-	public static int[] createAndInitializeArray(int size) {
-		if (size <= 0) {
-			throw new IllegalArgumentException("El tamaño del array debe ser mayor que cero.");
+	public static int[] createAndInitializeArray(int length) {
+		if (length < 0) {
+			System.out.println("El tamaño del array no puede ser negativo. Se creará un array vacío.");
+			return new int[0];
 		}
-
-		int[] array = new int[size];
-
-		for (int i = 0; i < size; i++) {
+		int[] array = new int[length];
+		for (int i = 0; i < array.length; i++) {
 			array[i] = i + 1;
 		}
-
 		return array;
 	}
 
 	public static void showInlineArray(int[] array) {
-		// Recorrer y mostrar los elementos del array
 		for (int i = 0; i < array.length; i++) {
-			if (i > 0) {
+			System.out.print(array[i]);
+
+			if (i < array.length - 1) {
 				System.out.print(" ");
 			}
-			System.out.print(array[i]);
 		}
-		System.out.println();
 	}
 }
